71273d8b3cca20a1e42e62e29c139d4aa66fa407
shadip01 <shadipkhadka01@gmail.com>
2025-02-22 09:45:33 -0500

    Added Vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+
+
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/realtor.ai/", 
+  base: '/realtor.ai/', 
   plugins: [react()],
   resolve: {
     alias: {
